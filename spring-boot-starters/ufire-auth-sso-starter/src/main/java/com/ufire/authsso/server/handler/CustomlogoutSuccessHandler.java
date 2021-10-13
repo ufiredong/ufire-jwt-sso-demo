fb6d81e5fcbba5b291eb7ad0c066f924a888e0f5
fengandong <ufiread@sina.com>
2021-10-13 17:38:03 +0800

    测试

--- spring-boot-starters/ufire-auth-sso-starter/src/main/java/com/ufire/authsso/server/handler/CustomlogoutSuccessHandler.java
+++ spring-boot-starters/ufire-auth-sso-starter/src/main/java/com/ufire/authsso/server/handler/CustomlogoutSuccessHandler.java
@@ -3,7 +3,6 @@ package com.ufire.authsso.server.handler;
 import com.ufire.authsso.server.properties.SsoServerCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
